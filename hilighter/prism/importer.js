b89f56dac748937aa63a4456a8fa224384a157f7
Ironboy <ironboy@users.noreply.github.com>
2025-04-16 17:19:22 +0200

    oh yeah

---
+++ hilighter/prism/importer.js
@@ -0,0 +1,63 @@
+let deps = { "abap": ["abap"], "abnf": ["abnf"], "actionscript": ["*javascript", "actionscript"], "ada": ["ada"], "agda": ["agda"], "al": ["al"], "antlr4": ["antlr4"], "apacheconf": ["apacheconf"], "apex": ["*clike", "*sql", "apex"], "apl": ["apl"], "applescript": ["applescript"], "aql": ["aql"], "arduino": ["*cpp", "arduino"], "arff": ["arff"], "armasm": ["armasm"], "arturo": ["arturo"], "asciidoc": ["asciidoc"], "asm6502": ["asm6502"], "asmatmel": ["asmatmel"], "aspnet": ["*markup", "*csharp", "aspnet"], "autohotkey": ["autohotkey"], "autoit": ["autoit"], "avisynth": ["avisynth"], "idl": ["idl"], "awk": ["awk"], "bash": ["bash"], "basic": ["basic"], "batch": ["batch"], "bbcode": ["bbcode"], "bicep": ["bicep"], "birb": ["*clike", "birb"], "bison": ["*c", "bison"], "bnf": ["bnf"], "brainfuck": ["brainfuck"], "brightscript": ["brightscript"], "bro": ["bro"], "bsl": ["bsl"], "c": ["*clike", "c"], "cfscript": ["*clike", "cfscript"], "chaiscript": ["*clike", "*cpp", "chaiscript"], "cil": ["cil"], "clike": ["clike"], "clojure": ["clojure"], "cmake": ["cmake"], "cobol": ["cobol"], "coffeescript": ["*javascript", "coffeescript"], "concurnas": ["concurnas"], "cooklang": ["cooklang"], "coq": ["coq"], "cpp": ["*c", "cpp"], "crystal": ["*ruby", "crystal"], "csharp": ["*clike", "csharp"], "cshtml": ["*markup", "*csharp", "cshtml"], "csp": ["csp"], "extras": ["*php", "extras"], "css": ["css"], "csv": ["csv"], "cue": ["cue"], "cypher": ["cypher"], "d": ["*clike", "d"], "dart": ["*clike", "dart"], "dataweave": ["dataweave"], "dax": ["dax"], "dhall": ["dhall"], "diff": ["diff"], "django": ["*templating", "django"], "file": ["file"], "docker": ["docker"], "dot": ["dot"], "ebnf": ["ebnf"], "editorconfig": ["editorconfig"], "eiffel": ["eiffel"], "ejs": ["*javascript", "*templating", "ejs"], "elixir": ["elixir"], "elm": ["elm"], "erb": ["*ruby", "*templating", "erb"], "erlang": ["erlang"], "etlua": ["*lua", "*templating", "etlua"], "formula": ["formula"], "factor": ["factor"], "false": ["false"], "rules": ["*clike", "rules"], "flow": ["*javascript", "flow"], "fortran": ["fortran"], "fsharp": ["*clike", "fsharp"], "ftl": ["*templating", "ftl"], "gap": ["gap"], "gcode": ["gcode"], "gdscript": ["gdscript"], "gedcom": ["gedcom"], "gettext": ["gettext"], "gherkin": ["gherkin"], "git": ["git"], "glsl": ["*c", "glsl"], "gml": ["*clike", "gml"], "gn": ["gn"], "module": ["module"], "go": ["*clike", "go"], "graphql": ["graphql"], "groovy": ["*clike", "groovy"], "haml": ["*ruby", "haml"], "handlebars": ["*templating", "handlebars"], "haskell": ["haskell"], "haxe": ["*clike", "haxe"], "hcl": ["hcl"], "hlsl": ["*c", "hlsl"], "hoon": ["hoon"], "hpkp": ["hpkp"], "hsts": ["hsts"], "http": ["http"], "ichigojam": ["ichigojam"], "icon": ["icon"], "format": ["format"], "idris": ["*haskell", "idris"], "iecst": ["iecst"], "ignore": ["ignore"], "inform7": ["inform7"], "ini": ["ini"], "io": ["io"], "j": ["j"], "java": ["*clike", "java"], "javadoc": ["*markup", "*java", "*javadoclike", "javadoc"], "javadoclike": ["javadoclike"], "javascript": ["*clike", "javascript"], "javastacktrace": ["javastacktrace"], "jexl": ["jexl"], "jolie": ["*clike", "jolie"], "jq": ["jq"], "templates": ["*javascript", "templates"], "jsdoc": ["*javascript", "*javadoclike", "*typescript", "jsdoc"], "json": ["json"], "json5": ["*json", "json5"], "jsonp": ["*json", "jsonp"], "jsstacktrace": ["jsstacktrace"], "jsx": ["*markup", "*javascript", "jsx"], "julia": ["julia"], "keepalived": ["keepalived"], "keyman": ["keyman"], "kotlin": ["*clike", "kotlin"], "kumir": ["kumir"], "kusto": ["kusto"], "latex": ["latex"], "latte": ["*clike", "*templating", "*php", "latte"], "less": ["*css", "less"], "lilypond": ["*scheme", "lilypond"], "script": ["script"], "liquid": ["*templating", "liquid"], "lisp": ["lisp"], "livescript": ["livescript"], "llvm": ["llvm"], "log": ["log"], "lolcode": ["lolcode"], "lua": ["lua"], "magma": ["magma"], "makefile": ["makefile"], "markdown": ["*markup", "markdown"], "templating": ["templating"], "markup": ["markup"], "mata": ["mata"], "matlab": ["matlab"], "maxscript": ["maxscript"], "mel": ["mel"], "mermaid": ["mermaid"], "mizar": ["mizar"], "mongodb": ["*javascript", "mongodb"], "monkey": ["monkey"], "moonscript": ["moonscript"], "n1ql": ["n1ql"], "n4js": ["*javascript", "n4js"], "hdl": ["hdl"], "naniscript": ["naniscript"], "nasm": ["nasm"], "neon": ["neon"], "nevod": ["nevod"], "nginx": ["nginx"], "nim": ["nim"], "nix": ["nix"], "nsis": ["nsis"], "objectivec": ["*c", "objectivec"], "ocaml": ["ocaml"], "odin": ["odin"], "opencl": ["*c", "opencl"], "openqasm": ["openqasm"], "oz": ["oz"], "parigp": ["parigp"], "parser": ["*markup", "parser"], "pascal": ["pascal"], "pascaligo": ["pascaligo"], "pcaxis": ["pcaxis"], "peoplecode": ["peoplecode"], "perl": ["perl"], "php": ["*templating", "php"], "phpdoc": ["*php", "*javadoclike", "phpdoc"], "uml": ["uml"], "plsql": ["*sql", "plsql"], "powerquery": ["powerquery"], "powershell": ["powershell"], "processing": ["*clike", "processing"], "prolog": ["prolog"], "promql": ["promql"], "properties": ["properties"], "protobuf": ["*clike", "protobuf"], "psl": ["psl"], "pug": ["*markup", "*javascript", "pug"], "puppet": ["puppet"], "pure": ["pure"], "purebasic": ["*clike", "purebasic"], "purescript": ["*haskell", "purescript"], "python": ["python"], "q": ["q"], "qml": ["*javascript", "qml"], "qore": ["*clike", "qore"], "qsharp": ["*clike", "qsharp"], "r": ["r"], "racket": ["*scheme", "racket"], "reason": ["*clike", "reason"], "regex": ["regex"], "rego": ["rego"], "renpy": ["renpy"], "rescript": ["rescript"], "rest": ["rest"], "rip": ["rip"], "roboconf": ["roboconf"], "robotframework": ["robotframework"], "ruby": ["*clike", "ruby"], "rust": ["rust"], "sas": ["sas"], "sass": ["*css", "sass"], "scala": ["*java", "scala"], "scheme": ["scheme"], "scss": ["*css", "scss"], "session": ["*bash", "session"], "smali": ["smali"], "smalltalk": ["smalltalk"], "smarty": ["*templating", "smarty"], "sml": ["sml"], "solidity": ["*clike", "solidity"], "soy": ["*templating", "soy"], "sparql": ["*turtle", "sparql"], "spl": ["spl"], "sqf": ["*clike", "sqf"], "sql": ["sql"], "squirrel": ["*clike", "squirrel"], "stan": ["stan"], "stata": ["*mata", "*java", "*python", "stata"], "stylus": ["stylus"], "supercollider": ["supercollider"], "swift": ["swift"], "systemd": ["systemd"], "cs": ["*templating", "*csharp", "cs"], "vb": ["*templating", "*vbnet", "vb"], "tap": ["*yaml", "tap"], "tcl": ["tcl"], "textile": ["*markup", "textile"], "toml": ["toml"], "tremor": ["tremor"], "tsx": ["*jsx", "*typescript", "tsx"], "tt2": ["*clike", "*templating", "tt2"], "turtle": ["turtle"], "twig": ["*templating", "twig"], "typescript": ["*javascript", "typescript"], "typoscript": ["typoscript"], "unrealscript": ["unrealscript"], "uorazor": ["uorazor"], "uri": ["uri"], "v": ["*clike", "v"], "vala": ["*clike", "vala"], "vbnet": ["*basic", "vbnet"], "velocity": ["*markup", "velocity"], "verilog": ["verilog"], "vhdl": ["vhdl"], "vim": ["vim"], "warpscript": ["warpscript"], "wasm": ["wasm"], "wiki": ["*markup", "wiki"], "wolfram": ["wolfram"], "wren": ["wren"], "xeora": ["*markup", "xeora"], "doc": ["*markup", "doc"], "xojo": ["xojo"], "xquery": ["*markup", "xquery"], "yaml": ["yaml"], "yang": ["yang"], "zig": ["zig"] };
+let themes = ["coy", "dark", "funky", "okaidia", "solarizedlight", "tomorrow", "twilight", "prism", "a11y-dark", "atom-dark", "base16-ateliersulphurpool.light", "cb", "coldark-cold", "coldark-dark", "coy-without-shadows", "darcula", "dracula", "duotone-dark", "duotone-earth", "duotone-forest", "duotone-light", "duotone-sea", "duotone-space", "ghcolors", "gruvbox-dark", "gruvbox-light", "holi-theme", "hopscotch", "lucario", "material-dark", "material-light", "material-oceanic", "night-owl", "nord", "one-dark", "one-light", "pojoaque", "shades-of-purple", "solarized-dark-atom", "synthwave84", "vs", "vsc-dark-plus", "xonokai", "z-touch"];
+
+let dir = import.meta.url.split('/').slice(0, -1).join('/');
+if (dir.split('/').pop() === 'src') { dir += '/..'; }
+
+function darkOrLight(className) {
+  let tempEl = document.createElement('div');
+  tempEl.classList.add(className);
+  tempEl.innerHTML = '<pre class="language-"></pre>';
+  document.body.append(tempEl);
+  let color = getComputedStyle(tempEl.querySelector('pre')).backgroundColor;
+  let [r, g, b] = color.split('(')[1].split(')')[0].split(',').map(x => +x);
+  let luma = 0.2126 * r + 0.7152 * g + 0.0722 * b;
+  tempEl.remove();
+  return luma > 128 ? 'light' : 'dark';
+}
+
+export async function importPrismLanguage(lang) {
+  lang === 'js' && (lang = 'javascript');
+  lang === 'ts' && (lang = 'typecript');
+  (lang === 'cs' || lang === 'c#') && (lang = 'csharp');
+  if (!deps[lang]) { return false; }
+  let importArr = ['*' + lang];
+  while (importArr.find(x => x.startsWith('*'))) {
+    let index = importArr.findIndex(x => x.startsWith('*'));
+    let lang = importArr.find(x => x.startsWith('*')).slice(1);
+    importArr.splice(index, 1, ...deps[lang]);
+  }
+  for (let lang of importArr) {
+    await import(`./langs/${lang}.js`);
+  }
+  return lang;
+}
+
+globalThis.themeContentCache = {};
+export async function importPrismTheme(themeName) {
+  const sleep = ms => new Promise(res => setTimeout(res, ms));
+  if (!themes.includes(themeName)) { return false; }
+  let themeContent = themeContentCache[themeName];
+  if (!themeContent) {
+    themeContentCache[themeName] = true;
+    let fetched = await (await fetch(`${dir}/themes/${themeName}.css`)).text();
+    themeContentCache[themeName] = fetched;
+  }
+  while (typeof themeContentCache[themeName] === 'boolean') {
+    await sleep(10);
+  }
+  themeContent = themeContentCache[themeName];
+
+  // ::-moz-selection breaks rule nesting in Sarari (2025-04-07, version 18.1)
+  // so remove all vendor prefixes on pseudoelements for the sake of Safari
+  themeContent = themeContent.replace(/::-\w*-/g, '::');
+
+  themeContent = themeContent.replace(/font-size[^;]*;/, '');
+  let className = 'prism-theme-' + themeName;
+  if (document.querySelector('head style#' + className)) { return darkOrLight(className); }
+  let styleEl = document.createElement('style');
+  styleEl.setAttribute('id', className);
+  styleEl.textContent = `.${className} {\n${themeContent}\n}`;
+  document.querySelector('head').append(styleEl);
+  return darkOrLight(className);
+}
