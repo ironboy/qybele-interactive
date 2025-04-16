a69fd56653ee61a558db4532fcdde0c91dfec300
Ironboy <ironboy@users.noreply.github.com>
2025-04-16 18:37:26 +0200

    minor

--- version20/hilite.js
+++ version20/hilite.js
@@ -3,13 +3,14 @@ import './hilighter/dist.js';
 export default function hilite(content) {
   //globalThis.__qybeleInteractivePrismCodeList(document.querySelector('#root'), markdown);
   $('pre[data-code-details]').each(function () {
-    let html = $(this).html()
+    let orgHtml = $(this).html();
+    let html = orgHtml
       .replace(/<br>/g, '\n')
       .replace(/℅/g, '&lt;')
       .replace(/℆/g, '&gt;');
     let settings = $(this).attr('data-code-details');
     html = `<pre data-code-details="${settings}">${html}</pre>`;
-    let div = $('<div></div>');
+    let div = $(`<div style="visibility:hidden">${orgHtml}</div>`);
     $(this).replaceWith(div);
     globalThis.__qybeleInteractivePrismCodeList(div[0], html, true);
   });
