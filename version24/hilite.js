418b26c1d6991b25d50f13f8c5b25ac729e45f03
Ironboy <ironboy@users.noreply.github.com>
2025-04-16 20:11:54 +0200

    again

--- version24/hilite.js
+++ version24/hilite.js
@@ -1,7 +1,6 @@
 import './hilighter/dist.js';
 
 export default function hilite(content) {
-  //globalThis.__qybeleInteractivePrismCodeList(document.querySelector('#root'), markdown);
   $('pre[data-code-details]').each(function () {
     let orgHtml = $(this).html();
     let html = orgHtml
