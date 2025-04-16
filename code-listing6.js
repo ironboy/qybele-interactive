35b48e14d4448f3164b6a5b90b8099d70370e1a0
Ironboy <ironboy@users.noreply.github.com>
2025-04-16 09:26:25 +0200

    again

--- code-listing6.js
+++ code-listing6.js
@@ -1,7 +1,7 @@
 function start() {
   const article = document.currentScript.parentElement.parentElement;
   const section = article.querySelector('section.module_content');
-  if (!section) { setTimeout(start, 10); }
+  if (!section) { setTimeout(start, 10); return; }
   console.log(section.innerHTML);
   //article.innerHTML += '<p>This is some extra content</p>';
 }
