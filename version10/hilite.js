e3ec2eefa5f4be4e46f039898fd05c785ec299b8
Ironboy <ironboy@users.noreply.github.com>
2025-04-16 17:23:57 +0200

    now

---
+++ version10/hilite.js
@@ -0,0 +1,5 @@
+export default function hilite(content) {
+  $('pre[data-code-details]').each(function () {
+    console.log("I SEE YA", this);
+  });
+}
--- version10/index.js
+++ version10/index.js
@@ -1,5 +1,7 @@
-import testAddContent from './testAddContent.js';
+// import testAddContent from './testAddContent.js';
+import hilite from './hilite.js';
 
 globalThis.__qybeleInteractiveMain = async function (content) {
-  testAddContent(content);
+  hilite(content);
+  // testAddContent(content);
 };
