d4215a293e866eb13f47876accf734a15fdffefd
Ironboy <ironboy@users.noreply.github.com>
2025-04-16 09:27:24 +0200

    ok

--- code-listing7.js
+++ code-listing7.js
@@ -1,5 +1,7 @@
 function start() {
-  const article = document.currentScript.parentElement.parentElement;
+  const article = document.currentScript;
+  console.log(article);
+  return;
   const section = article.querySelector('section.module_content');
   if (!section) { setTimeout(start, 10); return; }
   console.log(section.innerHTML);
