941a0da7033a910d1f8007b5370692187bb515ce
Ironboy <ironboy@users.noreply.github.com>
2025-04-20 22:19:05 +0200

    cleanup

--- version38/index.js
+++ version38/index.js
@@ -1,7 +1,5 @@
-// import testAddContent from './testAddContent.js';
 import hilite from './hilite.js';
 
 globalThis.__qybeleInteractiveMain = async function (content) {
   hilite(content);
-  // testAddContent(content);
 };
