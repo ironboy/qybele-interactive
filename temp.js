4b67550ee28bf9f3414d7b1d9806e5c74e611404
Ironboy <ironboy@users.noreply.github.com>
2025-04-28 07:27:50 +0200

    49

--- temp.js
+++
@@ -1,6 +0,0 @@
-let r = parent;
-if (!r.document.querySelector('[src="https://cdn.jsdelivr.net/gh/ironboy/qybele-interactive/fire.js"]')) {
-  let s = r.document.createElement('script');
-  s.setAttribute('src', 'https://cdn.jsdelivr.net/gh/ironboy/qybele-interactive/fire.js');
-  r.document.querySelector('head').append(s);
-}
