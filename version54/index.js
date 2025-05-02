dc16b37c832be8072c403c085835377a5e6b3e99
Ironboy <ironboy@users.noreply.github.com>
2025-05-02 12:29:59 +0200

    54

--- version54/index.js
+++ version54/index.js
@@ -5,7 +5,6 @@ globalThis.__qybeleInteractiveMain = async function (content) {
   if (hasRun) { return; }
   hasRun = true;
   content = content || $('section.module_content');
-  console.log("Check", content);
   hilite(content);
   let dir = import.meta.url.split('/').slice(0, -1).join('/');
   $('head').append(`<link rel="stylesheet" href="${dir}/css/tables-and-figures.css">`);
