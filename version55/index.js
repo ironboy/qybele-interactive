a85c31a30b46ddba07f85e3966b2b0207f42c792
Ironboy <ironboy@users.noreply.github.com>
2025-05-02 12:38:28 +0200

    55

--- version55/index.js
+++ version55/index.js
@@ -8,6 +8,16 @@ globalThis.__qybeleInteractiveMain = async function (content) {
   hilite(content);
   let dir = import.meta.url.split('/').slice(0, -1).join('/');
   $('head').append(`<link rel="stylesheet" href="${dir}/css/tables-and-figures.css">`);
+  // open external links in a new window
+  content[0] && content[0].addEventListener('click', e => {
+    let a = e.target.closest('a');
+    if (!a) { return; }
+    let href = a.getAttribute('href');
+    if (!href) { return; }
+    if (href.startsWith('http')) {
+      a.setAttribute('target', '_blank');
+    }
+  });
 };
 
 globalThis.__qybeleInteractiveMain();
