76171256f6ccd79d34d25a23bd78b6cd35b67287
Ironboy <ironboy@users.noreply.github.com>
2025-05-02 12:07:57 +0200

    53

--- version53/index.js
+++ version53/index.js
@@ -1,8 +1,14 @@
 import hilite from './hilite.js';
 
+let hasRun = false;
 globalThis.__qybeleInteractiveMain = async function (content) {
-  console.log("HI", content);
+  if (hasRun) { return; }
+  hasRun = true;
+  content = content || $('section.module_content');
+  console.log("Check", content);
   hilite(content);
   let dir = import.meta.url.split('/').slice(0, -1).join('/');
   $('head').append(`<link rel="stylesheet" href="${dir}/css/tables-and-figures.css">`);
 };
+
+globalThis.__qybeleInteractiveMain();
