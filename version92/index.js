aab17f3b76bac331d8dfe8f92b7fd280cc3278dc
Ironboy <ironboy@users.noreply.github.com>
2025-06-18 07:45:28 +0200

    92 - see if we can keep menu pos on restore

--- version92/index.js
+++ version92/index.js
@@ -1,9 +1,28 @@
 import hilite from './hilite.js';
 
+const sleep = ms => new Promise(res => setTimeout(res, ms));
+
+// fix for qybele triggering prism grammar error
+// when going to another article (not when loading fresh hard reload)
+// based on us replacing throw grammar error in dist with call to the function below
+globalThis._reloadOnGrammarError = () => {
+  localStorage.restoreJsResourceMenuScrollPosTo =
+    document.querySelector('#js-resource_menu').scrollTop;
+  location.replace();
+};
+
+if (localStorage.restoreJsResourceMenuScrollPosTo) {
+  let pos = localStorage.restoreJsResourceMenuScrollPosTo;
+  delete localStorage.restoreJsResourceMenuScrollPosTo;
+  while (!document.querySelector('#js-resource_menu')) {
+    await sleep(100);
+    document.querySelector('#js-resource_menu').scrollTop = pos;
+  }
+}
+// end fix
+
 const g = globalThis;
 g.__qybele_interactive_hilite = hilite;
-
-const sleep = ms => new Promise(res => setTimeout(res, ms));
 
 let hasRun = false;
 async function boot(content) {
